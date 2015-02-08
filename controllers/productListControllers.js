5674679de97e979d9449ee0ec7788a3b628f5f9e
Richard Rubio <rrubiorr81@gmail.com>
2015-02-08 00:02:11 -0500

    adding some notes-comments

--- controllers/productListControllers.js
+++ controllers/productListControllers.js
@@ -2,6 +2,7 @@
 angular.module("sportsStore")
     .controller("productListCtrl", function ($scope, $filter) {
         var selectedCategory = null;
+        //newCategory is the whole product order, passed on click...
         $scope.selectCategory = function (newCategory) {
             selectedCategory = newCategory;
         }
@@ -9,3 +10,9 @@ angular.module("sportsStore")
             return selectedCategory == null ||
             product.category == selectedCategory;
         } });
+
+//apparently when the user clicks on a category and the selectCategory method is called; we set the local variable
+//[selectedCategory]. Then the filter -somehow- reruns on the model -somehow modified- and lets only the products
+//belonging to the selected category...
+
+//The question i have is "how the filter knows it must be rerun, when only an internal variable changed [selectedCategory]...?"
