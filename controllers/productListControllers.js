da19b62dd5e804a18488eb46c9381a062c09ad91
Richard Rubio <rrubiorr81@gmail.com>
2015-02-07 23:24:43 -0500

    adding a second controller, to look up in a specific div; -- filtering the products on clicking categories

---
+++ controllers/productListControllers.js
@@ -0,0 +1,11 @@
+//adding a nre controller to the sportStore module...
+angular.module("sportsStore")
+    .controller("productListCtrl", function ($scope, $filter) {
+        var selectedCategory = null;
+        $scope.selectCategory = function (newCategory) {
+            selectedCategory = newCategory;
+        }
+        $scope.categoryFilterFn = function (product) {
+            return selectedCategory == null ||
+            product.category == selectedCategory;
+        } });
